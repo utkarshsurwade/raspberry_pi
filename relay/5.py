949773700a8aa855ba185702761eb16471156a6f
utkarshsurwade <utkarsh77777@gmail.com>
2016-06-24 23:32:11 +0530

    home automation

---
+++ relay/5.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO #library
+
+GPIO.setmode(GPIO.BCM) #gpio order scheme 
+
+GPIO.cleanup() #turn all channels to input 
+
+GPIO.setwarnings(False) #turn off warnings
+
+GPIO.setup(6,GPIO.OUT) #set gpio as output
+                        #now the relay detects pin 6 as output
+
+print "switch is now closed"
+
+GPIO.output(6,GPIO.LOW)
+
---
+++ relay/8a.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO #library
+
+GPIO.setmode(GPIO.BCM) #gpio order scheme(bcm stands for broadcom)
+
+GPIO.cleanup() #turn all channels to input
+
+GPIO.setwarnings(False) #turn off warnings
+
+GPIO.setup(26,GPIO.OUT) #set gpio as output
+                        #now the relay detects pin 26 as output
+
+print "switch is now open"
+
+GPIO.output(26,GPIO.HIGH) 
