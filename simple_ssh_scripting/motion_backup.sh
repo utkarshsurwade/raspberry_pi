5ca7605568de8a529ce3b1b569eff08f6d0d17cb
utkarshsurwade <utkarsh77777@gmail.com>
2016-07-15 21:24:36 +0530

    first sheel script

---
+++ simple_ssh_scripting/motion_backup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#shell scripts can be executed in many ways:type in terminal
+#1 bash "script_name.sh"
+#2 ./bla/blal/blal/script_name.sh (most preferred)[ownership is required]
+#3 when shabang preprocessor directive is not present then type in
+#sh script_name
+ 
+if sudo rsync -av /var/lib/motion utkarsh@192.168.0.105:~/Desktop #ssh keys for root user is required for this command to word
+then
+sudo rm -r /var/lib/motion/*
+else
+echo "motion was unable to shift"
+fi
